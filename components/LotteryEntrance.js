1b226eafbeb813a74107905875397ce1dc1dc481
JK1331 <jakubkubala3@gmail.com>
2022-09-08 10:46:36 +0200

    Styling

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -78,7 +78,7 @@ export default function LotteryEntrance() {
             icon: "bell"
         })
     }
-
+// Rendering function
     return ( 
         <div> 
             Hi from Lottery Entrance
